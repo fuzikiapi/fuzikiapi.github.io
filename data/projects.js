de0e7caa072602594b167d11bfc32274d469359f
Kadu Fuziki <59468628+fuzikiapi@users.noreply.github.com>
2026-07-07 06:12:18 -0300

    Create projects.js

---
+++ data/projects.js
@@ -0,0 +1,69 @@
+window.PROJECTS = [
+
+    {
+
+        title: "Nexhub",
+
+        description:
+            "Minha comunidade.",
+
+        image:
+            "assets/images/projects/kf.jpg",
+
+        technologies: [
+
+            "HTML",
+
+            "CSS",
+
+            "JavaScript"
+
+        ],
+
+        github:
+
+            "",
+
+        demo:
+
+            "",
+
+        status:
+
+            "Em desenvolvimento"
+
+    },
+
+    {
+
+        title: "NexHub",
+
+        description:
+            "Comunidade gamer.",
+
+        image:
+            "assets/images/projects/nexhub.jpg",
+
+        technologies: [
+
+            "Discord",
+
+            "JavaScript"
+
+        ],
+
+        github:
+
+            "",
+
+        demo:
+
+            "",
+
+        status:
+
+            "Ativo"
+
+    }
+
+];
