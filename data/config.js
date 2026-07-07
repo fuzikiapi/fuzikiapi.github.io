d9db07b397ac93acded6d425cf1b66efac72b107
Kadu Fuziki <59468628+fuzikiapi@users.noreply.github.com>
2026-07-07 05:41:02 -0300

    Update config.js

--- data/config.js
+++ data/config.js
@@ -1,1 +1,49 @@
+window.CONFIG = {
 
+    profile: {
+
+        name: "Kadu Fuziki",
+
+        tagline: "Creating digital experiences.",
+
+        avatar: "assets/images/avatar/avatar.png"
+
+    },
+
+    socials: {
+
+        github: "",
+
+        instagram: "",
+
+        discord: "",
+
+        riot: "",
+
+        youtube: "",
+
+        spotify: "",
+
+        email: ""
+
+    },
+
+    github: {
+
+        username: ""
+
+    },
+
+    discord: {
+
+        userId: "",
+
+        serverInvite: ""
+
+    },
+
+    language: "pt-BR",
+
+    timezone: "America/Sao_Paulo"
+
+};
